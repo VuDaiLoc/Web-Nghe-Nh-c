aa2f1040de89bbb796d44555ebc4e7774ffe1708
VuDaiLoc <locvu2912@gmail.com>
2026-04-13 21:10:37 +0700

    xoa

--- index.js
+++
@@ -1,3 +0,0 @@
-// server/index.js - Entry point của backend (alias của server.js)
-// File này được giữ để đúng cấu trúc commit theo kế hoạch
-require('./server');
--- middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-var isAdmin = (req, res, next) => {
-  if (req.isAuthenticated() && req.user && req.user.role === 'admin') {
-    return next();
-  }
-  return res.status(403).json({ success: false, message: 'Bạn không có quyền truy cập !' });
-};
-
-module.exports = { isAdmin };
